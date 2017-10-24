26cca9567d9577b2bb490285b63aaa98492d00af
Cory Shields <shieldscr88@gmail.com>
2017-10-23 22:50:25 -0400

    More tests

--- src/pencil.js
+++ src/pencil.js
@@ -1,7 +1,26 @@
 class Pencil {
+  
+  constructor(leadLength) {
+    this.leadLength = leadLength;
+    this.text = '';
+  }
+  
   write(text) {
+    for(let character of text) {
+      if(this.leadLength > 0) {
+        if(character !== ' ') {
+          if(character == character.toUpperCase()) {
+            this.leadLength -= 2;
+          } else {
+            this.leadLength--;
+          }
+        }
+        this.text += character;
+      }
+    }
     return text;
   }
+  
 }
 
 export { Pencil as default }
--- test/pencil.spec.js
+++ test/pencil.spec.js
@@ -1,7 +1,40 @@
 import Pencil from "../src/pencil.js";
 
-test('pencil can write', () => {
-  let pencil = new Pencil();
+let pencil;
 
-  expect(pencil.write('a')).toBe('a');
+beforeEach(() => {
+  pencil = new Pencil(1);
+});
+
+describe('pencil', () => {
+  test('can write', () => {
+    expect(pencil.write('a')).toBe('a');
+  });
+  
+  test('lead durability degrades by 1 when writing a single character', () => {
+    pencil.write('a');
+    expect(pencil.leadLength).toBe(0);
+  });
+  
+  test('no longer writes when it is out of lead', () => {
+    pencil.write('abc');
+    expect(pencil.text).toBe('a');
+  });
+  
+  test('can return what it wrote', () => {
+    pencil = new Pencil(5);
+    pencil.write('abc');
+    expect(pencil.text).toBe('abc');
+  });
+  
+  test('capital letters degrade lead by 2', () => {
+    pencil = new Pencil(5);
+    pencil.write('A');
+    expect(pencil.leadLength).toBe(3);
+  });
+  
+  test('spaces expend no lead', () => {
+    pencil.write(' ');
+    expect(pencil.leadLength).toBe(1);
+  });
 });
